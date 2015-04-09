59d764fa515dad3f89bbda645310e49a35fa544a
Bret Copeland <bret@atlantisflight.org>
2015-04-09 17:33:25 -0400

    Initial Commit.

---
+++ BosunSampleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using BosunReporter;
+
+namespace BosunSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collector = SetupCollector();
+            SampleRunner.RecordMetricsForever(collector);
+        }
+
+        private static MetricsCollector SetupCollector()
+        {
+            var options = new BosunOptions()
+            {
+                MetricsNamePrefix = "present.",
+                GetBosunUrl = () => new Uri("http://192.168.59.103:8070/"),
+                ThrowOnPostFail = true,
+                ReportingInterval = 5,
+                PropertyToTagName = tag => tag.ToLowerInvariant(),
+                DefaultTags = new Dictionary<string, string> { { "host", NameTransformers.Sanitize(Environment.MachineName.ToLower()) } }
+            };
+
+            return new MetricsCollector(options);
+        }
+    }
+}
---
+++ BosunSampleApp/SampleRunner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using BosunReporter;
+using BosunReporter.Metrics;
+
+namespace BosunSampleApp
+{
+    public static class SampleRunner
+    {
+        public static void RecordMetricsForever(MetricsCollector collector)
+        {
+            var rand = new Random();
+
+            // basic counter (no extra tags)
+            var myCounter = collector.CreateMetric<Counter>("my_counter", "units", "desc");
+
+            // counter with tags
+//            var successCounter = collector.CreateMetric("results", "units", "desc", new ResultCounter(ResultType.Success));
+
+            // metric group
+            var group = collector.GetMetricGroup<ResultType, ResultCounter>("results", "u", "d");
+            group.PopulateFromEnum();
+
+            // multi-aggregate gauge
+            var gauge = collector.CreateMetric<RandomGauge>("random", "stuff", "no desc");
+
+            // record on them
+            while (true)
+            {
+                myCounter.Increment();
+
+                var x = rand.NextDouble();
+                if (x < .1)
+                    group[ResultType.Error].Increment();
+                else if (x < .3)
+                    group[ResultType.Timeout].Increment();
+                else
+                    group[ResultType.Success].Increment();
+
+                gauge.Record(rand.NextDouble());
+
+                // slow down the infinite loop
+                Thread.Sleep(20);
+            }
+        }
+    }
+
+    // Metric types
+
+    public enum ResultType
+    {
+        Success,
+        Error,
+        Timeout,
+    }
+
+    public class ResultCounter : Counter
+    {
+        [BosunTag]
+        public readonly string Result;
+
+        public ResultCounter(ResultType result)
+        {
+            Result = result.ToString();
+        }
+    }
+
+    [GaugeAggregator(AggregateMode.Max)]
+    [GaugeAggregator(AggregateMode.Percentile, 0.95)]
+    [GaugeAggregator(AggregateMode.Median)]
+    [GaugeAggregator(AggregateMode.Min)]
+    public class RandomGauge : AggregateGauge
+    {
+    }
+}
